fccf100673f3068eb1f118c8c280a3e115586a23
Chinmayi Divakara <chinmayi365@gmail.com>
2016-04-27 17:53:50 -0700

    added Restapi to retrieve logs

--- RDocker/create.py
+++ RDocker/create.py
@@ -9,9 +9,7 @@ class containersClass(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         r = getContainer()
-        resp.status = falcon.HTTP_200
-      
-     
+        resp.status = falcon.HTTP_200  
         json_string = json.dumps(r)
         resp.body = json_string
         
@@ -29,6 +27,33 @@ class containerCreateClass(object):
         resp.body = json_string
        # resp.body = "done"
         return
+    
+#Resource class for starting the created container
+class containerStartClass(object):
+    
+    
+    def on_get(self,req,resp):
+        
+        print(req.params)
+        r = startContainer(req.params)
+        resp.status = falcon.HTTP_200
+        
+        resp.body = "".join(r)
+       # resp.body
+        return
+
+#Resource class to get the application logs from the deployed container
+class containerGetlogs(object):
+    
+    
+    def on_get(self,req,resp):
+        
+        print(req.params)
+        r = getlogs(req.params)
+        resp.status = falcon.HTTP_200
+        resp.stream  = r
+        resp.stream_len = r
+        yield
    
 #function that calls the docker containers API on unix socket   
 def getContainer():# add param cmd to send the command 
@@ -40,6 +65,89 @@ def getContainer():# add param cmd to send the command
 #function that calls the create_containers API on unix socket
 def createContainer(params):
     
+    if 'detach' in params:
+        params['detach'] = bool(params['detach'] )
+      
+    if 'stdin_open' in params:
+        params['stdin_open'] = bool( params['stdin_open'])
+    
+    if 'tty' in params:
+        params['tty'] = bool( params['tty'])
+   
+    if 'network_disabled' in params:
+        params['network_disabled'] = bool( params['network_disabled'])
+    
     c = Client(base_url = 'unix://var/run/docker.sock')
     ret = c.create_container(**params)
     return ret
+    
+    
+def startContainer(params):# add param cmd to send the command 
+    c = Client(base_url = 'unix://var/run/docker.sock')
+    cid = {}
+    cid["container"] = params['container']
+    print(cid)
+    
+    if 'logs' in params:
+        
+        if bool(params['logs']) is True:
+            
+            try:
+                del params['logs']
+            except KeyError:
+                pass
+            
+            if 'stdout' in params:
+                params['stdout'] = bool(params['stdout'])
+            
+            if 'stdin' in params:
+                params['stdin'] = bool(params['stdin'])
+                
+            if 'stream' in params:
+                params['stream'] = bool(params['stream'])
+    
+            if 'stderr' in params:
+                params['stderr'] = bool(params['stderr'])
+            
+            if 'timestamps' in params:
+                params['timestamps'] = bool(params['timestamps'])
+            
+            if 'tail' in params:
+                if isdigit(params['tail']):
+                    params['tail'] = int(params['tail'])
+                   
+            
+            container = c.start(**cid)
+            container = c.logs(**params)
+    else:
+        container = c.start(**cid)
+   
+    return container
+
+def getlogs(params):
+    
+    c = Client(base_url = 'unix://var/run/docker.sock')
+    
+    if 'stdout' in params:
+        params['stdout'] = bool(params['stdout'])
+    
+    if 'stdin' in params:
+        params['stdin'] = bool(params['stdin'])
+        
+    if 'stream' in params:
+        params['stream'] = bool(params['stream'])
+
+    if 'stderr' in params:
+        params['stderr'] = bool(params['stderr'])
+    
+    if 'timestamps' in params:
+        params['timestamps'] = bool(params['timestamps'])
+    
+    if 'tail' in params:
+        if isdigit(params['tail']):
+            params['tail'] = int(params['tail'])
+    
+    container = c.logs(**params)
+    
+    yield  container
+           
--- launch.py
+++ launch.py
@@ -6,7 +6,11 @@ api = application = falcon.API()
 
 class_containers = create.containersClass()
 class_createDocker = create.containerCreateClass()
+class_startContainer = create.containerStartClass()
+class_containerGetlogs = create.containerGetlogs()
 
 # add RestAPI route here and map the corresponding resource class
 api.add_route('/containers', class_containers )
 api.add_route('/run', class_createDocker)
+api.add_route('/start',class_startContainer)
+api.add_route('/logs',class_containerGetlogs)
